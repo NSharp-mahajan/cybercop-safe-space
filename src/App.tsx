8318f190d65316e91a00c7daef3e2e430d21e226
NSharp-mahajan <mahajannatansh@gmail.com>
2025-09-26 11:22:04 +0530

    Enhanced Landing page

--- src/App.tsx
+++ src/App.tsx
@@ -16,11 +16,8 @@ import Help from "./pages/Help";
 import CommunityReports from "./pages/CommunityReports";
 import Chat from "./pages/Chat";
 import ReportScam from "./pages/ReportScam";
-<<<<<<< HEAD
 import FraudNews from "./pages/FraudNews";
-=======
 import ScamMap from '@/pages/ScamMapAPI';
->>>>>>> 5b6b3d50bb913139edb2439a28777e8203ad8811
 import NotFound from "./pages/NotFound";
 import ChatWidget from "./components/ChatWidget";
 import DebugPanel from "./components/DebugPanel";
